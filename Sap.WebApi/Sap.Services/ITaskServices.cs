63cf08bbcad2b6527de5c6c521b184b5b3be9355
Loretech <88615874+Loretech@users.noreply.github.com>
2021-08-08 17:31:12 +0530

    API code commit

---
+++ Sap.WebApi/Sap.Services/ITaskServices.cs
@@ -0,0 +1,13 @@
+﻿using Sap.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sap.Services
+{
+    public interface ITaskServices
+    {
+        List<TaskViewModel> GetTasks();
+    }
+}
